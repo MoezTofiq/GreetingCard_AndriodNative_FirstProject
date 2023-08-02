0d2981b5bdc4c6d8497707835cbdca64c2bf2fbc
Moeez Tofiq <MoeezTofiq@outlook.com>
2023-08-02 14:23:05 +0500

    first commit

---
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.greetingcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.greetingcard.ui.theme.GreetingCardTheme
+
+class MainActivity : ComponentActivity() {
+//    the onCreate function is the main function of the app and calls the other functions for the app ( this is the main like in kotlin)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { // this is to define the layout through composable functions
+            // all functions marked with @composable can be called through
+            GreetingCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+/*
+Composable requirements:
+    need the Composable annotation on top of function
+    function name is capitalized
+    it can not return anything
+*/
+@Composable // this is to tell the kotlin compiler that the function below is used by jetpack compose
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface() { // came from alt+enter -> surround with widget -> sround with widget -> delete box and add widget
+        Text(
+            text = "Hello, my name is $name!",
+            modifier = modifier
+        )
+    }
+}
+
+// the function below is for previewing the function above without building the application.
+// @Preview tells andriod to show the composable function in andriod design view
+// showBackground adds a background to the output of the function
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GreetingCardTheme {
+        Greeting("Fucker")
+    }
+}
